15565cb97cbe1e767d07136c69a2ab8f2be09a2e
Maschell <Maschell@gmx.de>
2017-05-13 09:48:35 +0200

    Added SD-Access everywhere (patch by @Zarklord1)
    This does only work, if you disable the "no ios repload" option in the
    settings

--- ios_mcp/source/main.c
+++ ios_mcp/source/main.c
@@ -29,3 +29,17 @@ int _startMainThread(void)
     }
     return 0;
 }
+
+void patch_SD_access_check(void) {
+    __asm__ volatile(
+        ".thumb\n"
+        //clobbered instructions
+        "add r0, r7, r2\n"
+        //app.permissions.r2.mask seems to be 0xFFFFFFFFFFFFFFFF for every application
+        "ldr r1, =0x32\n"
+        "sub r3, r3, #7\n"
+        "strb r1, [r3]\n"
+        //this instruction was also clobbered but we use r1 so we do it after our patch stuff
+        "movs r1, #0\n"
+        "bx lr");
+}
